ecb68bfee0c9656d571092cdc9afb100212d4fca
Matt Baker <matthew.baker@mikesierra.net>
2015-12-16 17:17:49 +0000

    getting structure in

---
+++ tests/NumberTest.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of NumberTest
+ *
+ * @author matt
+ */
+class NumberTest extends PHPUnit_Framework_TestCase{
+    
+    public function testOneToNineteen() {
+        $number = new \mattb\numbers\Number();
+        $result = $number->getString($num);
+        
+        
+    }
+    
+}
